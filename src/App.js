9a8b2ce2d3a56f1362d43e078732cae77c10d72e
CodeSandbox <support@codesandbox.io>
2023-02-17 20:18:09 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Time to do some magic</h1>
+    </div>
+  );
+}
